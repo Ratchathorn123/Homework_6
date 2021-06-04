4cd4b54ba7227a9d16190ee43c83280bcbd84f72
Ratchathorn123 <79511320+Ratchathorn123@users.noreply.github.com>
2021-06-04 20:52:54 +0700

    Add files via upload

---
+++ Homework_6.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Homework_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            enum Difficulty
+        {
+            Easy,
+            Normal,
+            Hard
+        }
+        struct Problem
+        {
+            public string Message;
+            public int Answer;
+
+            public Problem(string message, int answer)
+            {
+                Message = message;
+                Answer = answer;
+            }
+        }
+        static Problem[] GenerateRandomProblems(int numProblem)
+        {
+            Problem[] randomProblems = new Problem[numProblem];
+            Random rnd = new Random();
+            int x, y;
+            for (int i = 0; i < numProblem; i++)
+            {
+                x = rnd.Next(50);
+                y = rnd.Next(50);
+                if (rnd.NextDouble() >= 0.5)
+                    randomProblems[i] =
+                        new Problem(string.Format("{0} + {1} = ?", x, y), x + y);
+                else
+                    randomProblems[i] =
+                        new Problem(string.Format("{0} - {1} = ?", x, y), x - y);
+            }
+            return randomProblems;
+        }
+        
+        
+    
+
